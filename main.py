2c7faf52a526c3c77868641d8c50f0005b3c677c
Aadiththa Karikaalan <adithyen1@gmail.com>
2025-08-23 21:45:05 +0530

    added al

--- main.py
+++ main.py
@@ -31,3 +31,11 @@ app.include_router(dashboard_router)
 
 # Serve static uploaded files
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
